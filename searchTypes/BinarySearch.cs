2045ab76a20a9f7c2a1ed76e2248ad561d8e429b
Devo2003 <93745734+Devo2003@users.noreply.github.com>
2023-12-04 23:52:52 -0600

    fixed the method that return the wronf index

--- searchTypes/BinarySearch.cs
+++ searchTypes/BinarySearch.cs
@@ -40,69 +40,73 @@ namespace searchTypes
 
             for (int i = 0; i < Nums.Length; i++)
             {
-                if (i == wantedValue)
+                if (Nums[i] == wantedValue)
                 {
                     Console.WriteLine("Got it! " + i);
-                    return i;  // Return the index if the wanted int is found
+                    return i;  // Return the int if the wanted index is found
                     
                 }
                 
             }
             Console.WriteLine("Where is it?");
-            return 0;  // Return 0 if the wanted int is not found
+            return 0;  // Return 0 if the wanted index is not found
 
         }
-        int low;
-        int high;
-
-        int mid;
+        
+        
         public int binarySearch()
         {
-            wantedValue = 4;
-            low = 0;
-            high = Nums.Length - 1;
+            Array.Sort(Nums);
 
-            while (low <= high)
-            {
-                mid = (low + high) / 2;
-
-                if (mid == wantedValue)
-                {
-                    Console.WriteLine("Found it! " + mid);
-                    return mid;  // Return the index if the wanted value is found
-                }
-                else if (mid < wantedValue)
-                {
-                    low = mid + 1;  // Continue searching in the right half
-                }
-                else
-                {
-                    high = mid - 1;  // Continue searching in the left half
-                }
-            }
-            Console.WriteLine("Couldn't find it");
-            return 0;  // Return 0 if the target is not found
-
-        }
-        public int InterpolationSearch()
-        {
+            wantedValue = 88;
             int low = 0;
             int high = Nums.Length - 1;
 
-            wantedValue = 95;
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
 
-            while (low <= high && wantedValue >= low && wantedValue <= high)
+                if (wantedValue == Nums[mid])
+                {
+                    Console.WriteLine("Found it! " + mid);
+                    return ++mid;  // Return the int if the wanted value is found
+                }
+                else if (wantedValue < Nums[mid])
+                {
+                    high = mid - 1;  // Continue searching in the left half of the array
+                }
+                else
+                {
+                    low = mid + 1;  // Continue searching in the right half of the array
+                    
+                }
+            }
+            Console.WriteLine("Couldn't find it");
+            return 0;  // Return 0 if the wanted value is not found
+
+        }
+
+       
+        public int InterpolationSearch()
+        {
+            Array.Sort(Nums);
+            int low = 0;
+            int high = Nums.Length - 1;
+
+            wantedValue = 88;
+
+            while (low <= high && wantedValue >= Nums[low] && wantedValue <= Nums[high])
             {
                 // Calculate the estimated position
-                int position = low + ((wantedValue - low) * (high - low) / (high - low));
+                int position = low + ((wantedValue - Nums[low]) * (high - low) / (Nums[high] - Nums[low]));
 
-                if (position == wantedValue)
+                if (Nums[position] == wantedValue)
                 {
                     Console.WriteLine("Found it! " + position);
                     return position;  // wanted value is found
                 }
                 // Search in the right half
-                if (position < wantedValue)
+                if (Nums[position] < wantedValue)
                 {
                     low = position + 1;
                 }
